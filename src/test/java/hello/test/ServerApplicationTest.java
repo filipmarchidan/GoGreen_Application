9caaef95f098f653467424ca9f8d8d67d22324cb
Jordy van der Tang <j.vandertang@student.tudelft.nl>
2019-02-20 22:49:53 +0100

    Slight update to ServerApplication Test and .pom to get jacoco up and running on my end

--- src/test/java/hello/test/ServerApplicationTest.java
+++ src/test/java/hello/test/ServerApplicationTest.java
@@ -1,15 +1,20 @@
+package hello.test;
 
+import hello.ServerApplication;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 @RunWith(SpringRunner.class)
-@SpringBootTest
+
+@SpringBootTest(classes={ServerApplication.class})
 public class ServerApplicationTest{
 
     @Test
     public void contextLoads() {
+        Assert.assertTrue(true);
     }
+
 
 }
