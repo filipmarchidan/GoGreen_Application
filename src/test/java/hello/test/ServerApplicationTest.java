ea0e9dd151617bcb76b1b2a5652340b92f5c3858
Jordy van der Tang <j.vandertang@student.tudelft.nl>
2019-02-21 12:20:40 +0100

    added /.idea back to .gitignore. removed the Assert true from the test since not relevant

--- src/test/java/hello/test/ServerApplicationTest.java
+++ src/test/java/hello/test/ServerApplicationTest.java
@@ -1,19 +1,18 @@
 package hello.test;
 
 import hello.ServerApplication;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-@RunWith(SpringRunner.class)
 
+
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes={ServerApplication.class})
 public class ServerApplicationTest{
 
     @Test
     public void contextLoads() {
-        Assert.assertTrue(true);
     }
 
 
