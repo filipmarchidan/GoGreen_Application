ab87848e1c812269dd0e85e64d5fa88f86713e0d
Mark Basting <M.J.Basting@student.tudelft.nl>
2019-03-03 10:55:20 +0100

    Fixed UI project

--- src/main/java/UI/Main.java
+++ src/main/java/UI/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package UI;
 
 
 import javafx.application.Application;
@@ -20,7 +20,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
 
         primaryStage.initStyle(StageStyle.TRANSPARENT);
 
