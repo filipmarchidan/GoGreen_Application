96222d5709155ad43abebb15c887baf53e233266
Giacomo Mazzola <gmazzola99@gmail.com>
2019-03-17 20:12:30 +0100

    Fix warnings

--- src/main/java/API/Activity.java
+++ src/main/java/API/Activity.java
@@ -1,47 +1,46 @@
 package API;
 
-import javax.persistence.*;
-import java.sql.Time;
-import java.time.LocalDateTime;
-import java.util.Date;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity // This tells Hibernate to make a table out of this class
 @Table(name = "activities")
 public class Activity {
     
     
-    public Integer getId() {
-        return id;
-    }
-    
-    public void setId(Integer id) {
-        this.id = id;
-    }
-    
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-    
+
     @Column(name = "user_id", nullable = false)
     private int user_id;
-    
+
     @Column(name = "activity_type", nullable = false)
     private String activity_type;
-    
+
     @Column(name = "CO2_savings", nullable = false)
     private int co2_savings;
-    
+
     @Column(name = "date_time", nullable = false)
     private String date_time;
-    
 
-    public Activity(){
 
-    }
-
+    /**
+     * creates an activity.
+     * @param user_id user_id
+     * @param activity_type activity_type
+     * @param CO2_savings co2_savings
+     * @param date_time date_time
+     */
     public Activity(int user_id, String activity_type, int CO2_savings, String date_time) {
         this.user_id = user_id;
         this.activity_type = activity_type;
@@ -49,7 +48,12 @@ public class Activity {
         this.date_time = date_time;
     }
 
-   /* public Integer getId() {
+    public Activity(){
+
+    }
+
+
+    /* public Integer getId() {
         return id;
     }
 
@@ -61,6 +65,7 @@ public class Activity {
         return user_id;
     }
 
+    
     public void setUser_id(int user_id) {
         this.user_id = user_id;
     }
@@ -89,6 +94,11 @@ public class Activity {
         this.date_time = date_time;
     }
 
+
+    /**
+     * returns the date and time.
+     * @return date
+     */
     public static String getDateTime() {
         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         Date date = new Date();
@@ -97,14 +107,18 @@ public class Activity {
     
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Activity activity = (Activity) o;
-        return user_id == activity.user_id &&
-            co2_savings == activity.co2_savings &&
-            Objects.equals(id, activity.id) &&
-            Objects.equals(activity_type, activity.activity_type) &&
-            Objects.equals(date_time, activity.date_time);
+        return user_id == activity.user_id
+                && co2_savings == activity.co2_savings
+                && Objects.equals(id, activity.id)
+                && Objects.equals(activity_type, activity.activity_type)
+                && Objects.equals(date_time, activity.date_time);
     }
     
    
