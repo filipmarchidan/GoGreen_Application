c9089826ef593d8815e815f59e83f21fd7f905f2
filipmarchidan <marchidanfilip@yahoo.com>
2019-03-18 00:23:22 +0100

    Fix error

--- src/main/java/API/UserService.java
+++ src/main/java/API/UserService.java
@@ -12,49 +12,31 @@ public class UserService{
     @Autowired
     private UserRepository userRepository;
 
-    public User createUser(User user) {
+    public User createUser(User user)
+    {
         return userRepository.save(user);
     }
-<<<<<<< HEAD
 
     public User getUserById(Integer userId)
     {
-=======
-    
-    public User getUserById(Integer userId) {
->>>>>>> a6563dfae96b5c2443ac7c5fa3a2bf819c65b71f
         return userRepository.findById(userId).get();
     }
-    
-    public Iterable<User> getAllUsers() {
+    public Iterable<User> getAllUsers()
+    {
         return userRepository.findAll();
     }
-    
-    public void deleteUser(Integer userId) {
+    public void deleteUser(Integer userId)
+    {
         userRepository.deleteById(userId);
     }
-    
-    /** Finds an existing user by ID and updates info.
-     *
-     * @param newEmail new email address
-     * @param newPassword new password
-     * @param userId userID to update
-     * @return the updated user
-     */
-    public User updateUser(String newEmail, String newPassword, Integer userId) {
+    public User updateUser(String newEmail, String newPassword, Integer userId)
+    {
         User userFromDB = userRepository.findById(userId).get();
         userFromDB.setEmail(newEmail);
         userFromDB.setPassword(newPassword);
         User updatedUser = userRepository.save(userFromDB);
         return updatedUser;
     }
-<<<<<<< HEAD
     public List<User> getUserByEmail(String email) { return userRepository.findByEmail(email); }
 
-=======
-    
-    public List<User> getUserByEmail(String email) {
-        return userRepository.findByEmail(email);
-    }
->>>>>>> a6563dfae96b5c2443ac7c5fa3a2bf819c65b71f
 }
