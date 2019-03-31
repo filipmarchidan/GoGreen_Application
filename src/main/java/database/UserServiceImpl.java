f521b90d78a01e3b2e0f3f0a14d0d8f16a1aa260
filipmarchidan <marchidanfilip@yahoo.com>
2019-03-31 18:45:13 +0200

    Coverage for API package is 93%

--- src/main/java/database/UserServiceImpl.java
+++ src/main/java/database/UserServiceImpl.java
@@ -2,6 +2,8 @@ package database;
 
 import database.entities.User;
 
+import java.util.List;
+
 public interface UserServiceImpl {
     
     User createUser(User user);
@@ -11,6 +13,8 @@ public interface UserServiceImpl {
     void deleteUser(Integer userId);
     
     User updateUser(User user, String newEmail, String newPassword);
-    
+
+    List<User> getAllUsers();
+
     User getUserByEmail(String email);
 }
